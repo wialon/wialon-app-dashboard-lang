74dd8af2275c617c4105b7ad63da81a99aa78dfd
Aleksey Shmigelski <shmi@gurtam.com>
2014-03-21 14:01:44 +0300

    ru-en lang added

--- README.md
+++ README.md
@@ -1,2 +1,11 @@
-wialon-app-dashboard-lang
-=========================
+Dashboard Internationalization
+===============================
+
+`lang` directory contains translation files for Dashboard app in specific format
+
+### Contribution
+Feel free to pull request with new language file or translation fixes for supported languages.  
+After merge, we'll update app as soon as possible.
+
+### License
+[The MIT License](../master/LICENSE)
---
+++ lang/en.js
@@ -0,0 +1,29 @@
+var TRANSLATIONS = {
+	"login" : "",
+	"Login error" : "",
+	"initialize sdk" : "",
+	"Hello" : "",
+
+	"Today": "",
+	"Yesterday": "",
+	"Week": "",
+	"Month": "",
+	"Custom": "",
+	"Previous": "",
+	"Next": "",
+
+	"No Data Available.": "No data available.",
+	"Add widget": "",
+	"Cancel": "",
+	"Unit": "Units",
+	"Unit group": "",
+
+	"Show units:": "",
+	"Skip zeroth values": "Skip zero values",
+	"N/A": "",
+
+	'Oh! Unfortunately, you can\'t login': 'Incorrect login data.',
+	'Oh! %%module%% didn\'t load!': 'Error loading the app.',
+	'Oh! Unfortunately, we can\'t load SDK': 'Error loading the app.',
+	'Session has been lost': 'Session lost.'
+};
